da63697dc31314fc9a91b8f5993cce3ff0f35eb5
Nand Vinchhi <58524154+NandVinchhi@users.noreply.github.com>
2019-12-31 08:45:46 +0530

    Update RockPaperScissors.jl

--- RockPaperScissors.jl
+++ RockPaperScissors.jl
@@ -4,11 +4,21 @@ println("Try your luck against the computer and get a chance to be the victor.")
 while true
     println("")
     println("____________________________________________________________________________")
+
+    computer_score = 0
+    player_score = 0
+    i = 0
+    while i < 3
     println("")
     println("Enter 'rock', 'paper' or 'scissors' to commence. Choose wisely! :")
     
     x = lowercase(readline())
     
-    x == "rock" || x == "scissors" || x == "paper" ? (println(""); println("computer is processing... "); println(""); y = rand(["rock", "paper", "scissors"]); println("The computer chose " * y * "."); println(x == y ? "Draw game." : (x == "rock" && y == "scissors") || (x == "scissors" && y == "paper") || (x == "paper" && y == "rock") ? "Congratulations! You Win." : ":( The computer Won. Better luck Next Time!"); println(""); println("____________________________________________________________________________"); println(""); println("Enter 'Q' to quit and any other key to play again."); a = readline(); a == "Q" ? break : println("")) : println("Please enter a valid choice.")
-   
+    x == "rock" || x == "scissors" || x == "paper" ? (println(""); println("computer is processing... "); println(""); y = rand(["rock", "paper", "scissors"]); println("The computer chose " * y * "."); x == y ? (println("Draw game."); i += 1; println(""); println("____________________________________________________________________________"); println("")) : (x == "rock" && y == "scissors") || (x == "scissors" && y == "paper") || (x == "paper" && y == "rock") ? (println("You Win! +1 points for player.") ; player_score += 1; println("Your score: " * string(player_score)); println("Computer score: " * string(computer_score)); i += 1; println(""); println("____________________________________________________________________________"); println("")) : (println(":( The computer Won. +1 points for computer.") ; computer_score += 1; println("Your score: " * string(player_score)); println("Computer score: " * string(computer_score)); i += 1; println(""); println("____________________________________________________________________________"); println("") )) : println("Please enter a valid choice.")
+     
+    end
+    player_score > computer_score ? println("Congratulations, You win this match!") : player_score == computer_score ? println("Draw Match") : println("Game Over. The Computer Won. Better luck nex time!")
+    println("")
+    println("Enter 'Q' to quit and any other key to play again."); a = readline(); a == "Q" ? break : println("")  
 end
+
