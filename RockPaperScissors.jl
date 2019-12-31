b273f6c87803de3df2a5c54b1a09cf50330104ec
Nand Vinchhi <58524154+NandVinchhi@users.noreply.github.com>
2019-12-31 11:47:16 +0530

    Update RockPaperScissors.jl

--- RockPaperScissors.jl
+++ RockPaperScissors.jl
@@ -15,9 +15,9 @@ while true
     x = lowercase(readline())
     
     x == "rock" || x == "scissors" || x == "paper" ? (println(""); println("computer is processing... "); println(""); y = rand(["rock", "paper", "scissors"]); println("The computer chose " * y * "."); x == y ? (println("Draw game."); println("Your score: " * string(player_score)); println("Computer score: " * string(computer_score)); i += 1; println(""); println("____________________________________________________________________________"); println("")) : (x == "rock" && y == "scissors") || (x == "scissors" && y == "paper") || (x == "paper" && y == "rock") ? (println("You Win! +1 points for player.") ; player_score += 1; println("Your score: " * string(player_score)); println("Computer score: " * string(computer_score)); i += 1; println(""); println("____________________________________________________________________________"); println("")) : (println(":( The computer Won. +1 points for computer.") ; computer_score += 1; println("Your score: " * string(player_score)); println("Computer score: " * string(computer_score)); i += 1; println(""); println("____________________________________________________________________________"); println("") )) : println("Please enter a valid choice.")
-     
+    player_score == 2 || computer_score == 2 ? break : println("")
     end
-    player_score > computer_score ? println("Congratulations, You win this match!") : player_score == computer_score ? println("Draw Match") : println("Game Over. The Computer Won. Better luck nex time!")
+    player_score > computer_score ? println("Congratulations, You win this match!") : player_score == computer_score ? println("Draw Match") : println("Game Over. The Computer Won. Better luck next time!")
     println("")
     println("Enter 'Q' to quit and any other key to play again."); a = readline(); a == "Q" ? break : println("")  
 end
